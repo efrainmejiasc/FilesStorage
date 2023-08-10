78fda7041ef00b730261d3a1bc1bd5f0bbfe7b5f
EfrainMejiasC <efrainmejiasc@hotmail.com>
2023-08-10 14:01:47 -0400

    Update

--- FilesStorage/FilesStorageApi/Program.cs
+++ FilesStorage/FilesStorageApi/Program.cs
@@ -1,3 +1,10 @@
+using AutoMapper;
+using FilesStorageModels.DataModels;
+using FilesStorageShared.AutoMapperProfile;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,62 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = string.Empty;
+#if DEBUG
+connectionString = builder.Configuration.GetConnectionString("DefaultConnectionLocal");
+#else
+connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+#endif
+
+builder.Services.AddDbContext<FilesStorageContext>(options => options.UseSqlServer(connectionString));
+
+//AGREGAR SWAGGER
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "FILE STORAGE TO AZURE API", Version = "v1" });
+    options.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["controller"]}_{e.ActionDescriptor.RouteValues["action"]}");
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer "
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+
+});
+
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new AutoMapperProfile());
+});
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
 
 var app = builder.Build();
 
---
+++ FilesStorage/FilesStorageModels/DataModels/FilesStorageContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesStorageModels.DataModels
+{
+    public class FilesStorageContext : DbContext
+    {
+        public FilesStorageContext(DbContextOptions<FilesStorageContext> options)
+         : base(options)
+        {
+        }
+
+        //public virtual DbSet<Roles> Roles { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer(EngineData.ConnectionDb);
+        //    }
+
+        //}
+    }
+}
